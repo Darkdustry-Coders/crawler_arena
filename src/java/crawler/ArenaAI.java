54c355e9461a20e3e04010e213e72d1726f05cd2
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2022-05-07 15:15:31 +0300

    reznya

---
+++ src/java/crawler/ArenaAI.java
@@ -0,0 +1,42 @@
+package crawler_arena;
+
+import mindustry.ai.types.GroundAI;
+import mindustry.entities.Units;
+import mindustry.gen.Building;
+import mindustry.gen.Hitboxc;
+import mindustry.gen.Teamc;
+
+import static mindustry.Vars.tilesize;
+
+public class ArenaAI extends GroundAI {
+
+    // TODO улучшить
+
+    @Override
+    public void updateUnit() {
+        if (Units.invalidateTarget(target, unit.team, unit.x, unit.y, Float.MAX_VALUE)) {
+            target = null;
+        }
+
+        if (retarget()) {
+            target = target(unit.x, unit.y, unit.range(), true, true);
+        }
+
+        boolean rotate = false, shoot = false;
+
+        if (!Units.invalidateTarget(target, unit, unit.range()) && unit.hasWeapons()) {
+            rotate = true;
+            shoot = unit.within(target, unit.type.weapons.first().bullet.range() + (target instanceof Building b ? b.block.size * tilesize / 2f : ((Hitboxc) target).hitSize() / 2f));
+
+            unit.movePref(vec.set(target).sub(unit).limit(unit.speed()));
+        }
+
+        unit.controlWeapons(rotate, shoot);
+        faceTarget();
+    }
+
+    @Override
+    public Teamc target(float x, float y, float range, boolean air, boolean ground) {
+        return Units.closestTarget(unit.team, x, y, range, u -> u.checkTarget(air, ground), t -> ground);
+    }
+}
---
+++ src/java/crawler/ReinforcementAI.java
@@ -0,0 +1,29 @@
+package crawler_arena;
+
+import arc.math.Mathf;
+import arc.math.geom.Vec2;
+import mindustry.entities.units.AIController;
+import mindustry.gen.Call;
+
+import static mindustry.Vars.tilesize;
+import static mindustry.Vars.world;
+
+public class ReinforcementAI extends AIController {
+
+    @Override
+    public void updateUnit() {
+        unit.moveAt(new Vec2().trns(Mathf.atan2(world.width() * 4 - unit.x, world.height() * 4 - unit.y), unit.speed()));
+
+        if (world.width() * tilesize / 2f - unit.x < 120f) {
+            Call.payloadDropped(unit, unit.x, unit.y);
+        }
+
+        if (unit.x > world.width() * 7) {
+            Call.unitDespawn(unit);
+        }
+
+        if (unit.moving()) {
+            unit.lookAt(unit.vel().angle());
+        }
+    }
+}
